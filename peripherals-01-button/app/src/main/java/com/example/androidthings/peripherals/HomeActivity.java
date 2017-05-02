d01aa0927299c35d3c1642312923e49c80a79169
Dave Smith <smithdave@google.com>
2017-05-02 15:27:04 -0600

    Initial peripheral lab steps
    Change-Id: Ia497bdf734a220acce95fab7bcd8146b649e467b
    Signed-off-by: Dave Smith <smithdave@google.com>

---
+++ peripherals-01-button/app/src/main/java/com/example/androidthings/peripherals/HomeActivity.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidthings.peripherals;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+
+public class HomeActivity extends Activity {
+    private static final String TAG = "HomeActivity";
+    private static final String BUTTON_PIN_NAME = "BCM21";
+
+    // GPIO connection to button input
+    private Gpio mButtonGpio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PeripheralManagerService service = new PeripheralManagerService();
+        Log.d(TAG, "Available GPIO: " + service.getGpioList());
+
+        try {
+            // Create GPIO connection.
+            mButtonGpio = service.openGpio(BUTTON_PIN_NAME);
+
+            // Configure as an input, trigger events on every change.
+            mButtonGpio.setDirection(Gpio.DIRECTION_IN);
+            mButtonGpio.setEdgeTriggerType(Gpio.EDGE_BOTH);
+            // Value is true when the pin is LOW
+            mButtonGpio.setActiveType(Gpio.ACTIVE_LOW);
+            // Register the event callback.
+            mButtonGpio.registerGpioCallback(mCallback);
+        } catch (IOException e) {
+            Log.w(TAG, "Error opening GPIO", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Close the button
+        if (mButtonGpio != null) {
+            mButtonGpio.unregisterGpioCallback(mCallback);
+            try {
+                mButtonGpio.close();
+            } catch (IOException e) {
+                Log.w(TAG, "Error closing GPIO", e);
+            }
+        }
+    }
+
+    private GpioCallback mCallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            try {
+                Log.i(TAG, "GPIO changed, button " + gpio.getValue());
+            } catch (IOException e) {
+                Log.w(TAG, "Error reading GPIO");
+            }
+
+            // Return true to keep callback active.
+            return true;
+        }
+    };
+}
---
+++ peripherals-03-driver/app/src/main/java/com/example/androidthings/peripherals/HomeActivity.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2017, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ package com.example.androidthings.peripherals;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.contrib.driver.button.ButtonInputDriver;
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+
+public class HomeActivity extends Activity {
+    private static final String TAG = "HomeActivity";
+    private static final String BUTTON_PIN_NAME = "BCM21";
+    private static final String LED_PIN_NAME = "BCM6";
+
+    // Driver for the GPIO button
+    private ButtonInputDriver mButtonInputDriver;
+    // GPIO connection to LED output
+    private Gpio mLedGpio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PeripheralManagerService service = new PeripheralManagerService();
+        Log.d(TAG, "Available GPIO: " + service.getGpioList());
+
+        try {
+            // Initialize button driver to emit SPACE key events
+            mButtonInputDriver = new ButtonInputDriver(
+                    BUTTON_PIN_NAME,
+                    Button.LogicState.PRESSED_WHEN_LOW,
+                    KeyEvent.KEYCODE_SPACE);
+            // Register with the framework
+            mButtonInputDriver.register();
+        } catch (IOException e) {
+            Log.e(TAG, "Error opening button driver", e);
+        }
+
+        try {
+            mLedGpio = service.openGpio(LED_PIN_NAME);
+            // Configure as an output.
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            Log.w(TAG, "Error opening GPIO", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Unregister the driver and close
+        if (mButtonInputDriver != null) {
+            mButtonInputDriver.unregister();
+            try {
+                mButtonInputDriver.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing Button driver", e);
+            }
+        }
+
+        // Close the LED.
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing GPIO", e);
+            }
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_SPACE) {
+            // Turn on the LED
+            setLedValue(true);
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_SPACE) {
+            // Turn off the LED
+            setLedValue(false);
+            return true;
+        }
+
+        return super.onKeyUp(keyCode, event);
+    }
+
+    /**
+     * Update the value of the LED output.
+     */
+    private void setLedValue(boolean value) {
+        try {
+            mLedGpio.setValue(value);
+        } catch (IOException e) {
+            Log.e(TAG, "Error updating GPIO value", e);
+        }
+    }
+}
